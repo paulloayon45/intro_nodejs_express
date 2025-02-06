9cdb03f15274bd27c223dd3176ee12feba5264b3
John Paul <paulloayon6406@gmail.com>
2025-02-06 08:43:47 -0800

    Adding new route for display

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const items = ['Apple','Banana','Orange'];
+
+app.get('/items', (req, res) =>{
+    res.json(items);
+});
 
 app.use(express.json());
 
